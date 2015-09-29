35f4cf4cd0108abc1e9d7a7a27bfd6ffbc50cef1
Froghouse <tomas.landberg@gmail.com>
2015-09-29 12:44:12 +0200

    Added a setup script

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ln -s .config/mpd ~/.config/mpd
+ln -s .config/termite ~/.config/termite
+ln -s .i3 ~/.i3
+ln -s .i3blocks.conf ~/.i3blocks.conf
+ln -s .i3status.conf ~/.i3status.conf
+
+mkdir -p /home/fimas/.config/mpd/database
+touch /home/fimas/.config/mpd/log
+
